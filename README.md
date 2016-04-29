6dbd7e378834b827e526a5179e1405d77e27ce88
Mikel Eizagirre <meizagirre@irontec.com>
2016-04-29 09:27:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Demo de app con Crdova desarrollada durante el curso de Ionic Framework
+## Para poner en marcha el proyecto
+
+git clone https://github.com/irontec/demo-cordova-curso-ionic.git
+cd demo-cordova-curso-ionic
+ionic state restore
+ionic serve
+
+## Diapositivas utilizadas durante el curso
+
+Introducción a Ionic: http://irontec.github.io/slides-ionic-intro/
+Introducción a AngularJS: http://irontec.github.io/slides-angularjs-introduccion/
+Buenas prácticas con AngularJS: http://irontec.github.io/slides-angularjs-buenas-practicas
+Herramientas y componentes de Ionic: http://irontec.github.io/slides-ionic-tools
+Instalación y primeros pasos con Ionic: http://irontec.github.io/slides-ionic-installation
+Resumen de los visto: http://irontec.github.io/slides-ionic-recap
+Introducción a Gulp: http://irontec.github.io/slides-gulp
---
+++ www/js/app.js
@@ -0,0 +1,53 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+.controller('MainCtrl', function($cordovaCamera, $cordovaNetwork, $interval) {
+  var vm = this;
+
+  $interval(function() {
+    console.log('MainCtrl activated: ' + $cordovaNetwork.isOnline());
+  },1000);
+  
+  vm.openCamera = function() {
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+	  correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+    });
+  }
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
